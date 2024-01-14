909428489c2a823c48066999a85c1aa85b255f3c
Petr Karnakov <pkarnakov@gmail.com>
2024-01-14 00:53:48 -0500

    wasm: rm particles, add mask

--- src/poisson_wasm.cpp
+++ src/poisson_wasm.cpp
@@ -1,14 +1,15 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
-#include <stdint.h>
-#include <stdlib.h>
 
 #include <array>
 #include <chrono>
 #include <cmath>
+#include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <memory>
 #include <sstream>
@@ -26,26 +27,29 @@ using Clock = std::chrono::steady_clock;
 using Adam = optimizer::Adam<Scal>;
 
 struct Scene {
-  int width = 800;
-  int height = 800;
+  int width = 512;
+  int height = 512;
   int Nx = 128;
   int epochs_per_frame = 25;
   int max_nlvl = 4;
   Scal lr = 1e-3;
   Scal uref_k = 2;
-  std::array<Scal, 2> ulim = {-1, 1};
+  std::array<Scal, 2> ulim = {-0.8, 0.8};
 
   // Control flags.
   bool is_pause = false;
+  bool is_mouse_down = false;
 
   // Solver state.
-  Matrix<Scal> uref;  // Reference solution.
-  Matrix<Scal> rhs;   // Right-hand side.
+  Matrix<Scal> uref;                            // Reference solution.
+  Matrix<Scal> rhs;                             // Right-hand side.
   std::vector<std::unique_ptr<Var<M>>> var_uu;  // Multigrid components.
   std::vector<Tracer<M, Extra>> uu;  // Tracers of multigrid components.
   Tracer<M, Extra> u;                // Sum of multigrid components.
   NodeOrder<Extra> order;
   Tracer<Scal, Extra> loss;
+  std::unique_ptr<Var<M>> var_mask;
+  Tracer<M, Extra> mask;
 
   // Optimizer.
   std::unique_ptr<Adam> optimizer;
@@ -57,27 +61,25 @@ struct Scene {
   std::chrono::time_point<Clock> time_prev;
 
   // Data buffers.
-  std::vector<Vect> particles;
   std::vector<uint32_t> bitmap;
 
   // Visualization.
+  std::vector<char> status_string = {0};
 };
+
+int Select(Scal u, int min, int max) {
+  return std::min<int>(max,
+                       std::max<int>(min, std::round((1 - u) * min + u * max)));
+}
+
+template <class T>
+T Clip(T u, T min, T max) {
+  return std::min<T>(max, std::max<T>(min, u));
+}
 
 std::shared_ptr<Scene> g_scene;
 
 static void InitScene(Scene& scene) {
-  // Init particles.
-  const int n = 1000;
-  scene.particles.resize(n);
-  Scal phi = 0;
-  for (size_t i = 0; i < scene.particles.size(); ++i) {
-    using std::cos;
-    using std::sin;
-    const Scal r = 0.05 + phi * 0.01;
-    phi += 10 / (n * r);
-    scene.particles[i] = {0.5 + r * cos(phi), 0.5 + r * sin(phi)};
-  }
-
   const size_t Nx = scene.Nx;
   const Scal hx = 1. / Nx;
 
@@ -100,34 +102,32 @@ static void InitScene(Scene& scene) {
   };
 
   scene.rhs = eval_lapl(uref);
-  int nx = Nx;
   auto& var_uu = scene.var_uu;
   auto& uu = scene.uu;
-  // Create variables for multigrid levels.
-  for (int i = 0; i < scene.max_nlvl; ++i) {
-    const auto name = "u" + std::to_string(i + 1);
-    var_uu.emplace_back(std::make_unique<Var<M>>(M::zeros(nx), name));
-    uu.emplace_back(*var_uu.back());
-    nx /= 2;
-    if (nx <= 4) {
-      break;
+  {  // Create variables for multigrid levels.
+    int nx = Nx;
+    for (int i = 0; i < scene.max_nlvl; ++i) {
+      const auto name = "u" + std::to_string(i + 1);
+      var_uu.emplace_back(std::make_unique<Var<M>>(M::zeros(nx), name));
+      uu.emplace_back(*var_uu.back());
+      nx /= 2;
+      if (nx <= 4) {
+        break;
+      }
     }
+    auto u = uu.back();
+    for (size_t i = uu.size() - 1; i > 0;) {
+      --i;
+      u = interpolate(u) + uu[i];
+    }
+    scene.u = u;
   }
-  std::cout << "multigrid levels: ";
-  for (auto& var_u : var_uu) {
-    auto& m = var_u->value();
-    std::cout << "(" << m.nrow() << "," << m.ncol() << ") ";
-  }
-  std::cout << std::endl;
-
-  auto u = uu.back();
-  for (size_t i = uu.size() - 1; i > 0;) {
-    --i;
-    u = interpolate(u) + uu[i];
-  }
-  scene.loss = mean(sqr(eval_lapl(u) - scene.rhs));
+  // Create variable for mask.
+  scene.var_mask = std::make_unique<Var<M>>(M::zeros(Nx), "mask");
+  scene.mask = Tracer<M, Extra>(*scene.var_mask);
+  scene.loss = mean(sqr(eval_lapl(scene.u) - scene.rhs)) +
+               mean(sqr(scene.u * scene.mask / sqr(hx)));
   scene.order = scene.loss.GetFowardOrder();
-  scene.u = u;
 
   scene.time_prev = Clock::now();
 
@@ -140,10 +140,16 @@ static void InitScene(Scene& scene) {
       auto ms = msdur.count();
       const Scal throughput =
           (ms > 0 ? 1e-3 * scene.rhs.size() * scene.epochs_per_frame / ms : 0);
-      printf(
-          "epoch=%5d, loss=%8.6e, throughput=%.3fM cells/s, u:[%.3f,%.3f], \n",
-          epoch, scene.loss.value(), throughput, scene.u.value().min(),
-          scene.u.value().max());
+      auto print = [&](char* buf, size_t bufsize) -> int {
+        return std::snprintf(
+            buf, bufsize,
+            "epoch=%5d, loss=%.4e<br>throughput=%.3fM cells/s, u:[%.3f,%.3f]",
+            epoch, scene.loss.value(), throughput, scene.u.value().min(),
+            scene.u.value().max());
+      };
+      auto& s = scene.status_string;
+      s.resize(print(nullptr, 0) + 1);
+      print(s.data(), s.size());
     }
   };
 
@@ -174,63 +180,81 @@ void UpdateScene() {
   if (scene.is_pause) {
     return;
   }
+  {
+    /*
+    auto& state = scene.optimizer->state();
+    // Clear optimizer state, to adapt to changing mask.
+    state.vv.clear();
+    state.mm.clear();
+    */
+  }
   scene.optimizer->Run(scene.opt_config, scene.opt_vars, scene.opt_grads,
                        scene.update_grads, scene.callback);
-  auto& particles = scene.particles;
-  for (size_t i = 0; i < particles.size(); ++i) {
-    auto& p = particles[i];
-    p[0] += 0.02;
-    p[1] += 0.01;
-    if (p[0] > 1) {
-      p[0] -= 1;
-    }
-    if (p[1] > 1) {
-      p[1] -= 1;
-    }
-  }
   const auto& u = scene.u.value();
+  const auto& mask = scene.mask.value();
   scene.bitmap.resize(u.size());
   const auto& ulim = scene.ulim;
-  const Scal c0[] = {0.85, 0.5, 0.07};
-  const Scal c1[] = {0.97, 0.97, 0.97};
-  const Scal c2[] = {0.34, 0.17, 0.54};
-  auto linear = [&](Scal x, Scal x0, Scal x1, Scal u0, Scal u1) {
+  auto linear = [](Scal x, Scal x0, Scal x1, Scal u0, Scal u1) {
     return ((x1 - x) * u0 + (x - x0) * u1) / (x1 - x0);
   };
-  auto color = [&](Scal v, int k) -> uint8_t {
-    return 255 * (v < 0.5 ? linear(v, 0, 0.5, c0[k], c1[k])
-                          : linear(v, 0.5, 1, c1[k], c2[k]));
+  const Scal c3[] = {0, 0.95, 0.6};  // Green.
+  auto color = [linear](Scal v, int k) {
+    const Scal c0[] = {0.34, 0.17, 0.54};  // Blue.
+    const Scal c1[] = {0.97, 0.97, 0.97};  // White.
+    const Scal c2[] = {0.85, 0.5, 0.07};   // Orange.
+    return (v < 0.5 ? linear(v, 0, 0.5, c0[k], c1[k])
+                    : linear(v, 0.5, 1, c1[k], c2[k]));
   };
-  for (size_t i = 0; i < u.size(); ++i) {
-    const Scal v = std::max<Scal>(
-        0, std::min<Scal>(1, (u[i] - ulim[0]) / (ulim[1] - ulim[0])));
-    const uint8_t r = color(v, 0);
-    const uint8_t g = color(v, 1);
-    const uint8_t b = color(v, 2);
-    scene.bitmap[i] = (0xff << 24) | (b << 16) | (g << 8) | r;
+  auto blend = [](Scal c, Scal ca, Scal a) {  //
+    return c * (1 - a) + ca * a;
+  };
+  auto round = [](Scal c) -> uint8_t {
+    return std::max<Scal>(0, std::min<Scal>(255, std::round(c * 255)));
+  };
+  for (size_t i = 0; i < u.nrow(); ++i) {
+    for (size_t j = 0; j < u.ncol(); ++j) {
+      const Scal vu = std::max<Scal>(
+          0, std::min<Scal>(1, (u(i, j) - ulim[0]) / (ulim[1] - ulim[0])));
+      const Scal vmask = mask(i, j);
+      const uint8_t r = round(blend(color(vu, 0), c3[0], vmask * 0.5));
+      const uint8_t g = round(blend(color(vu, 1), c3[1], vmask * 0.5));
+      const uint8_t b = round(blend(color(vu, 2), c3[2], vmask * 0.5));
+      scene.bitmap[j * u.ncol() + i] = (0xff << 24) | (b << 16) | (g << 8) | r;
+    }
   }
   CopyToCanvas(scene.bitmap.data(), u.ncol(), u.nrow());
 }
 
-extern "C" {
-int GetParticles(uint16_t* data, int max_size) {
-  const int entrysize = 2;
-  int i = 0;
+void DrawCircle(Scal x, Scal y) {
   auto& scene = *g_scene;
-  const auto& particles = scene.particles;
-  for (auto p : particles) {
-    if (i + entrysize > max_size) {
-      break;
+  auto& mask = scene.var_mask->value();
+  const int ic = Select(x, 0, mask.nrow() - 1);
+  const int jc = Select(1 - y, 0, mask.ncol() - 1);
+  const int w = 3;
+  auto kernel = [](int dx, int dy) -> Scal {  //
+    return Clip<Scal>(0, 1, 2 * (1 - std::sqrt(sqr(dx) + sqr(dy)) / w));
+  };
+  const int i0 = Clip<int>(ic - w, 0, mask.nrow());
+  const int i1 = Clip<int>(ic + w, 0, mask.nrow());
+  const int j0 = Clip<int>(jc - w, 0, mask.ncol());
+  const int j1 = Clip<int>(jc + w, 0, mask.ncol());
+  for (int i = i0; i < i1; ++i) {
+    for (int j = j0; j < j1; ++j) {
+      const Scal a = kernel(i - ic, j - jc);
+      mask(i, j) = Clip<Scal>(a + mask(i, j) * (1 - a), 0, 1);
     }
-    data[i + 0] = p[0] * scene.width;
-    data[i + 1] = p[1] * scene.height;
-    i += entrysize;
   }
-  return i;
 }
-void SendKeyDown(char keysym) {
-  (void)keysym;
+
+void ResetOptimizer() {
+  auto& scene = *g_scene;
+  auto& state = scene.optimizer->state();
+  // Clear optimizer state, to adapt to changing mask.
+  state.vv.clear();
+  state.mm.clear();
 }
+
+extern "C" {
 int GetBitmapWidth() {
   return g_scene->Nx;
 }
@@ -239,19 +263,31 @@ int GetBitmapHeight() {
   return g_scene->Nx;
 }
 
+void SendKeyDown(char keysym) {
+  if (keysym == 'o') {
+    ResetOptimizer();
+  }
+}
+
 void SendMouseMotion(Scal x, Scal y) {
-  (void)x;
-  (void)y;
+  auto& scene = *g_scene;
+  if (!scene.is_mouse_down) {
+    return;
+  }
+  DrawCircle(x, y);
 }
 
 void SendMouseDown(Scal x, Scal y) {
-  (void)x;
-  (void)y;
+  auto& scene = *g_scene;
+  scene.is_mouse_down = true;
+  DrawCircle(x, y);
 }
 
 void SendMouseUp(Scal x, Scal y) {
   (void)x;
   (void)y;
+  auto& scene = *g_scene;
+  scene.is_mouse_down = false;
 }
 
 void Init() {
@@ -265,8 +301,8 @@ void SetPause(int flag) {
   g_scene->is_pause = flag;
 }
 
-const char* GetMouseMode() {
-  return "mode";
+char* GetStatusString() {
+  return g_scene->status_string.data();
 }
 }  // extern "C"
 
